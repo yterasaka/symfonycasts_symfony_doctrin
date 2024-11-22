d0b3d2b8d8ef6a0281f86c03ded71ccbcdf19506
Yuki Terasaka <yuki.terasaka@gmail.com>
2024-11-22 19:53:20 +0900

    Add 09

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
+use App\Repository\VinylMixRepository;
 
 class VinylController extends AbstractController
 {
@@ -35,11 +36,11 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-
-        $mixes = $this->mixRepository->findAll();
+        $mixes = $mixRepository->findAll();
+        // dd($mixes);
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
