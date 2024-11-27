4653daa7a27009c0ff78c8493153ad1ad0adc3ff
Yuki Terasaka <yuki.terasaka@gmail.com>
2024-11-27 11:04:40 +0900

    Add 19

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
